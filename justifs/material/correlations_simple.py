7e204bbccdc61fcd0529eeca747a37adcd670ade
Thamme Gowda <tg@isi.edu>
2020-11-04 19:02:34 -0800

    add MATERIAL results

--- justifs/material/correlations_simple.py
+++ justifs/material/correlations_simple.py
@@ -11,13 +11,10 @@ import pandas as pd
 log.basicConfig(level=log.INFO)
 
 def main(**args):
-
+    alpha = 0.05  # significance testing
     df = pd.read_table('tables/Material-Joel-AQWV-MAP-2B2C2S.tsv', index_col=False).dropna()
-    #unwanted = ['Year', 'System']
-    #df.drop(columns=unwanted, inplace=True)
+
     experiments = df.groupby('expt')['expt'].count()
-    experiments.to_csv('tables/material-exp-sizes.tsv', sep='\t')
-    #experiments.to_excel(writer, sheet_name=f'Experiments')
 
     mt_metrics = ['BLEU', 'MacroF1', 'MicroF1', 'ChrF1', 'BLEURTMean', 'BLEURTMedian']
     ir_metrics = ['mAQWV', 'MAP']
@@ -28,14 +25,22 @@ def main(**args):
     for corr_name, corr_func in corr_methods.items():
         for exp_name, n_exps in experiments.iteritems():
             exp_df = df[df['expt'] == exp_name]
-            for ir_metric in ir_metrics:
-                row = [exp_name, corr_name, ir_metric]
-                res.append(row)
-                for mt_metric in mt_metrics:
+            inner_delim = ' | '
+            row = [exp_name, corr_name, inner_delim.join(ir_metrics)]
+            res.append(row)
+
+            for mt_metric in mt_metrics:
+                cell_complex = []
+                for ir_metric in ir_metrics:
                     corr_val, p_val = corr_func(exp_df[mt_metric], exp_df[ir_metric])
-                    row.append('%.3f|%.2f' % (corr_val, p_val))
+                    corr_val = f'{corr_val:.3f}'
+                    if p_val >= alpha:
+                        corr_val += '*'  # * means NOT significant i.e. p_val >= alpha
+                    cell_complex.append(corr_val)
+                row.append(inner_delim.join(cell_complex))
+
     #print(res)
-    out_file = 'tables/material-correlations.tsv'
+    out_file = 'tables/material-correlations-simpl.tsv'
     log.info(f"Storing data at {out_file}")
     res_df = pd.DataFrame(data=res, columns=columns)
     res_df.to_csv(out_file, sep='\t')
